04e0525a8565fd95fb81d94920f3ac37501c9de3
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-04-29 10:44:01 -0300

    header | OK

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import styles from "./header.module.scss";
 
 export default function Header() {
-	return <header></header>;
+	return (
+		<header className={styles.header}>
+			<picture>
+				<source srcSet="assets/icons/gudi.svg" media="(max-width: 552px)" />
+
+				<img
+					className={styles.logo}
+					src="assets/icons/gudi-lema.svg"
+					alt="Gudi Logo"
+				/>
+			</picture>
+
+			<nav className={styles.menu}>
+				<ul>
+					<li>
+						<a href="#sobre">Sobre</a>
+					</li>
+
+					<li>
+						<a href="#beneficios">Benefícios</a>
+					</li>
+
+					<li>
+						<a href="#contato">Contato</a>
+					</li>
+
+					<li className="btn">
+						<a href="#agendar">Agendar</a>
+					</li>
+				</ul>
+			</nav>
+
+			<button className={styles.btnMenuMobile}>
+				<div className="line" />
+				<div className="line" />
+				<div className="line" />
+			</button>
+		</header>
+	);
 }
