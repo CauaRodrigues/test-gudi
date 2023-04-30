87f87e83ab90f882e2478f927612d8706b004ff3
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-04-30 16:31:19 -0300

    gallery | OK

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,7 @@ import Header from "@components/Header";
 import Hero from "@components/Hero";
 import InfoSection from "@components/InfoSection";
 import CardsStates from "@components/CardsStates";
+import Gallery from "@components/Gallery";
 import { data } from "./data";
 
 function Home() {
@@ -33,6 +34,7 @@ function Home() {
 			</InfoSection>
 
 			<CardsStates />
+			<Gallery />
 		</>
 	);
 }
---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,45 @@
+import "./gallery.styles.scss";
+
+export default function Gallery() {
+	return (
+		<section className="container--gallery">
+			<div className="title--gallery">
+				<span className="hashtag fw-md">use a hashtag #brasilisverigudi</span>
+				<h2 className="fw-bd">Nosso mural de Experiências</h2>
+			</div>
+
+			<div className="photos-gallery">
+				<img
+					className="photo"
+					src="assets/images/gallery-image1@2x.png"
+					alt="Homem brincando com um bebê"
+				/>
+				<img
+					className="photo"
+					src="assets/images/gallery-image2@2x.png"
+					alt="Grupo de crianças pulando"
+				/>
+				<img
+					className="photo"
+					src="assets/images/gallery-image3@2x.png"
+					alt="Homem tirando foto com cachorro"
+				/>
+				<img
+					className="photo"
+					src="assets/images/gallery-image4@2x.png"
+					alt="Selfie de um homem"
+				/>
+				<img
+					className="photo"
+					src="assets/images/gallery-image5@2x.png"
+					alt="Selfie de uma mulher ciclista"
+				/>
+				<img
+					className="photo"
+					src="assets/images/gallery-image6@2x.png"
+					alt="Selfie de grupo de mulheres sorrindo"
+				/>
+			</div>
+		</section>
+	);
+}
